4665a332ce45ad6d3efe0d589247b8647f5e908d
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-10-07 00:18:37 -0500

    Added the Hero2 into the Hero file, deleted Hero2.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,11 @@
 // src/pages/Home.jsx
 import React from 'react';
 import { Hero, Education, Products, SupportMission } from '../components/Home';
-import Hero2 from '../components/Home/Hero2';
 
 const Home = () => {
   return (
     <div>
-      {/* <Hero /> */}
-      <Hero2 />
+      <Hero />
       {/* <Education /> */}
       {/* <Products /> */}
       {/* <SupportMission /> */}
