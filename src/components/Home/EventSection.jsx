51ad692bf7f109116255c6a377cad8c454683431
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2024-10-10 13:17:04 -0500

    Tablet completed. The only  thing remaining is this section is text size updates

--- src/components/Home/EventSection.jsx
+++ src/components/Home/EventSection.jsx
@@ -31,57 +31,59 @@ function EventSection() {
 
 
                     {/* TEXT  */}
-                    <div className="flex flex-col justify-between">
+                    <div className="flex flex-col justify-between
+                                    md:pr-8">
                         <h2 className='text-4xl font-bold mt-8'>Ready To Join Our Latest Upcoming <span className='text-vBrightOrange'>Events?</span></h2>
                         <p className='text-xl mt-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id beatae possimus alias architecto eaque numquam nemo dolorem tempore eum unde, quia tempora corporis quis repudiandae.</p>
                         <button className='flex justify-center bg-vBrightOrange text-white px-4 py-3 mb-8 rounded-3xl mt-8 w-2/3'>Discover More <ArrowForwardIcon className='text-white ml-2' /></button>
                     </div>  
 
-                    {/* Div3: First image in first column, first row */}
-                        <div className="col-start-1 col-end-2 row-start-1 row-end-2">
-                            <img className="w-full h-full object-cover rounded-t-xl" src="/images/events/paint.jpg" alt="Image 1" />
-                        </div> 
-
-
-                
-                    {nextEvent ? (
-                        <div className="relative bg-softOrange p-5 flex items-start justify-start text-lef mb-8 rounded-b-xl">
-                            {/* XYZ attached to the event */}
-                            <div className="hidden xl:block bg-vBrightOrange px-8 py-2 rounded-full absolute right-[-6rem] top-1/2 transform -translate-y-1/2 -rotate-90 text-md font-bold text-white">
-                                {formatDate(nextEvent.date)}
-                            </div>
-
-                            {/* Event Data */}
-                            <div>
-                                <div className="location__and__time flex mb-4 pt-5 text-xs">
-                                    <div className="time flex items-center">
-                                        <AccessTimeIcon className='text-vBrightOrange' />
-                                        <p className="pl-2">{nextEvent.time}</p>
-                                    </div>
-                                    <div className="location flex items-center ml-4">
-                                        <LocationOnIcon className='text-vBrightOrange' />
-                                        <p className="pl-2">{nextEvent.location}</p>
-                                    </div>
-                                </div>
-                                <h3 className="font-bold mb-4 text-2xl">{nextEvent.title}</h3>
-                                <button className='border-none bg-transparent cursor-pointer'>
-                                View More <ArrowForwardIcon className='text-vBrightOrange' />
-                                </button>
-                            </div>
+                    <div className="md:grid grid-cols-2 gap-0 h-full mb-4">
+                        {/* Div3: First image in the first column */}
+                        <div className="h-full">
+                            <img className="w-full h-full object-cover rounded-t-xl md:rounded-b-xl" src="/images/events/paint.jpg" alt="Image 1" />
                         </div>
+
+                        {/* Event Data in the second column */}
+                        {nextEvent ? (
+                            <div className="relative bg-softOrange p-5 flex flex-col items-start justify-start text-left mb-8 rounded-b-xl h-full   
+                                                md:rounded-t-xl">
+                                {/* Event Data */}
+                                <div>
+                                    <div className="location__and__time flex mb-4 pt-5 text-xs">
+                                        <div className="time flex items-center">
+                                            <AccessTimeIcon className='text-vBrightOrange' />
+                                            <p className="pl-2">{nextEvent.time}</p>
+                                        </div>
+                                        <div className="location flex items-center ml-4">
+                                            <LocationOnIcon className='text-vBrightOrange' />
+                                            <p className="pl-2">{nextEvent.location}</p>
+                                        </div>
+                                    </div>
+                                    <h3 className="font-bold mb-4 text-2xl">{nextEvent.title}</h3>
+                                    <button className='border-none bg-transparent cursor-pointer'>
+                                        View More <ArrowForwardIcon className='text-vBrightOrange' />
+                                    </button>
+                                </div>
+                            </div>
                         ) : (
-                            <div>No upcoming events</div>  // Fallback if no event is found
+                            <div>No upcoming events</div> // Fallback if no event is found
                         )}
+                    </div>
+
+                    <div className="md:grid grid-cols-2 gap-0 h-full mb-4">
 
                         {/* Div5: Second image in the second row, first column */}
-                        <div className="col-start-1 col-end-2 row-start-2 row-end-3">
-                            <img className="w-full h-full object-cover rounded-t-xl" src="/images/events/jenga.jpg" alt="Image 2" />
+                        <div className="h-full">
+                            <img className="w-full h-full object-cover rounded-t-xl md:rounded-b-xl" src="/images/events/jenga.jpg" alt="Image 2" />
                         </div>
 
 
-                        <div className="bg-softGreen w-full h-full flex items-start justify-start text-left p-5 mb-8 rounded-b-xl">
+                        <div className="bg-softGreen w-full h-full flex items-start justify-start text-left p-5 mb-8 rounded-b-xl
+                         md:rounded-t-xl">
                             {nextNonDuplicateEvent ? (
-                                <div className="relative bg-softDarkGreen pt-5 flex items-start justify-start text-left">
+                                <div className="relative bg-softDarkGreen pt-5 flex items-start justify-start text-left
+                                                 md:rounded-t-xl">
                                 {/* XYZ attached to the event, visible only on screens 1440px and above */}
                                 <div className="hidden xl:block bg-vDarkGreen px-8 py-2 rounded-full absolute right-[-6rem] top-1/2 transform -translate-y-1/2 -rotate-90 text-md font-bold text-white">
                                     {formatDate(nextNonDuplicateEvent.date)}
@@ -110,7 +112,7 @@ function EventSection() {
                                 <div>No upcoming events</div>  // Fallback if no event is found
                             )}
                         </div>
-
+                    </div>
 
 
 
